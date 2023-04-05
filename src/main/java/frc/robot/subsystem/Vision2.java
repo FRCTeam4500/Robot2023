2754e556991d2fd123fdf4478f43ae07ec2985b4
VimalBuckley <92557966+VimalBuckley@users.noreply.github.com>
2023-04-05 13:38:58 -0500

    More Vision Work

--- src/main/java/frc/robot/subsystem/Vision2.java
+++ src/main/java/frc/robot/subsystem/Vision2.java
@@ -1,14 +1,22 @@
 package frc.robot.subsystem;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.VisionConstants;
 import frc.robot.component.hardware.LimelightVisionComponent;
 
 public class Vision2 extends SubsystemBase{
     private LimelightVisionComponent camera1;
-    
+
     public Vision2(LimelightVisionComponent vision1) {
         camera1 = vision1;
     }
 
+    public boolean hasValidTargets() {
+        return camera1.hasValidTargets();
+    }
+
+    public double depthToTarget() {
+        return (VisionConstants.TARGET_HEIGHT - VisionConstants.VISION_HEIGHT) / Math.tan(Math.abs(camera1.getVerticalOffsetFromCrosshair()));
+    }
 
 }
